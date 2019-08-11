a735ec154b706829e2e7ea39ee203eb1e8b2b5c8
Luis García <luis_grdz@hotmail.com>
2019-08-10 21:26:10 -0500

    01.-Primer ejemplo de typescript

---
+++ 01-typescript/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var nombre = "peter";
+var numero = 123;
+var booleano = true;
+var hoy = new Date();
+hoy = new Date('2020-10-21');
+var cualquiera;
+cualquiera = nombre;
+cualquiera = numero;
+cualquiera = booleano;
+cualquiera = hoy;
---
+++ 01-typescript/app.ts
@@ -0,0 +1,25 @@
+
+let nombre:string = "peter";
+let numero:number = 123;
+let booleano:boolean = true;
+
+let hoy:Date = new Date();
+
+hoy = new Date('2020-10-21');
+
+let cualquiera:any;
+
+cualquiera = nombre;
+cualquiera = numero;
+cualquiera = booleano;
+cualquiera = hoy;
+
+let spiderman = {
+    npmbre: "peter",
+    edad: 20
+}
+
+spiderman = {
+    npmbre: "juan",
+    edad: 30
+}
