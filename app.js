6b5c4424234a5f323e6bfd62160aead56fad8a20
Luis Alberto García Rodríguez <luis_grdz@hotmail.com>
2020-12-29 18:50:33 -0600

    vaciando el archivo de trabajo

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
 "use strict";
 (function () {
-    const retirarDinero = (montoRetirar) => {
-        let dineroActual = 1000;
-        return new Promise((resolve, reject) => {
-            if (montoRetirar > dineroActual) {
-                reject('No hay suficientes fondos');
-            }
-            else {
-                dineroActual -= montoRetirar;
-                resolve(dineroActual);
-            }
-        });
-    };
-    retirarDinero(500)
-        .then(montoActual => console.log(`Me quedan ${montoActual}`))
-        .catch(err => console.log(err));
 })();
